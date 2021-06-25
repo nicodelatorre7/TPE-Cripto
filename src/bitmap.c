23a97eff6ef58042cb718014da8af61c4d27dc21
juanmoliva <olivajuan12@gmail.com>
2021-06-25 18:31:18 -0300

    Bitmap and matrix notation processing

---
+++ src/bitmap.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "bitmap.h"
+
+
+
+
+int read_image(FILE *fp, BMPImage **image, char * filename){
+
+    *image = malloc(sizeof(BMPImage));
+    if(*image == NULL) {
+        printf("LoadImageError: Malloc error.\n");
+        return -1;
+    }
+    printf("1: %s\n",filename);
+    printf("1: %d\n",strlen(filename));
+    memset((*image)->filename, '\0', sizeof((*image)->filename));
+    memcpy((*image)->filename,filename,strlen(filename));
+    //strcpy((*image)->filename, filename);
+    printf("2: %s\n",(*image)->filename);
+    
+    // header
+    rewind(fp);
+    int header_num_read = fread(&((*image)->header), 1, sizeof((*image)->header), fp);
+    printf("header_num_read: %d\n", header_num_read);
+    if(header_num_read != sizeof((*image)->header)) {
+        printf("Load_image: error reading bmp header.\n");
+        return -1;
+    }
+    int header_info_num_read = fread(&((*image)->info_header), 1, sizeof((*image)->info_header), fp);
+    printf("header_info_num_read: %d\n", header_info_num_read);
+    if(header_info_num_read != sizeof((*image)->info_header)) {
+        printf("Load_image: error reading bmp info header.\n");
+        return -1;
+    }
+
+    printf("image offset: %u \n", (*image)->header.data_offset);
+    printf("image width: %u \n", (*image)->header.reserved);
+    printf("image width: %u \n", (*image)->info_header.width_px);
+    printf("image height: %u \n", (*image)->info_header.height_px);
+    int32_t image_size = ((*image)->info_header.width_px)* ((*image)->info_header.height_px);
+
+    int untilOffsetl = (*image)->header.data_offset - (sizeof((*image)->header) + sizeof((*image)->info_header));
+    printf("UNTIL NUMBER SIZE: %d\n", untilOffsetl);
+    (*image)->until_offset = malloc(sizeof(uint8_t) * untilOffsetl);
+
+    int untilOffset_num_read = fread((*image)->until_offset, 1, untilOffsetl, fp);
+    printf("untilOffset_num_read: %d\n", untilOffset_num_read);
+    if(untilOffset_num_read != untilOffsetl) {
+        printf("Load_image: error reading data until offset.\n");
+        return -1;
+    }
+
+    // image body
+    fseek(fp, (*image)->header.data_offset, SEEK_SET);
+    (*image)->body = malloc(sizeof(uint8_t) * image_size);
+    if ((*image)->body == NULL){
+        printf("Load_image: error allocating body\n");
+        return -1;
+    }
+
+    printf("image size: %d\n", image_size);
+    printf("Body read:\n");
+
+    // Read image data
+    int num_read = fread((*image)->body, 1, image_size, fp);
+    printf("num_read: %d\n", num_read);
+    /*if (num_read != image_size){
+        printf("Load_image: error reading image data\n");
+        return -1;
+    }*/
+
+    //print_image((*image)->body, (*image)->info_header.width_px, (*image)->info_header.height_px);
+
+    fclose(fp);
+
+    return 0;
+}
+
+
+int fill_body_from_matrix(BMPImage *image, uint8_t  ** body , mat_representation *mat_array, size_t arr_size){
+    int32_t image_width = image->info_header.width_px;
+    int32_t image_height = image->info_header.height_px;
+
+    int size = arr_size*4;
+    *body = malloc(sizeof(uint8_t) * size);
+    if( *body == NULL ) {
+        return -1;
+    }
+
+    for (int i=0; i < (int) arr_size; i++) {
+
+        (*body)[(2 *(image_height/2 - ( i / (image_height/2) )) - 1)* 2*(image_width/2) + 2*( i % (image_width/2) )] = mat_array[i].x_byte;
+
+        (*body)[(2 *(image_height/2 - ( i / (image_height/2) )) - 1)* 2*(image_width/2) + (2*( i % (image_width/2) ) + 1)] = mat_array[i].w_byte;
+
+        (*body)[((2 *(image_height/2 - ( i / (image_height/2) )) - 1) - 1)* 2*(image_width/2) + 2*( i % (image_width/2) )] = mat_array[i].v_byte;
+
+        (*body)[((2 *(image_height/2 - ( i / (image_height/2) )) - 1) - 1)* 2*(image_width/2) + (2*( i % (image_width/2) ) + 1)] = mat_array[i].u_byte;
+
+    }
+
+
+    return size;
+}
+
+
+int fill_matrix_array(BMPImage *image, mat_representation ** mat_array, size_t * arr_size){
+    uint8_t *body = image->body;
+    int32_t image_width = image->info_header.width_px;
+    int32_t image_height = image->info_header.height_px;
+    int count = 0;
+    *arr_size = image_height*image_width / 4;
+    
+
+    *mat_array = malloc(sizeof(mat_representation) * (*arr_size) );
+    
+
+    uint8_t body_matrix[image_height][image_width];
+
+
+    for(int c = image_height*image_width -1 ; c>=0; c--) {
+        int row = (c / image_height);
+        int col = c % image_width;
+        body_matrix[row][col] = body[c];
+    }
+
+    for(int rowA = image_height-1; rowA > 0; rowA -= 2) {
+        int rowB = rowA - 1;
+        int squares = image_width / 2;
+        
+        int offset = 0;
+        while( squares > 0 ) {
+            (*matrix_array)[count].x_byte = body_matrix[rowA][offset];
+            (*matrix_array)[count].w_byte = body_matrix[rowA][offset+1];
+            (*matrix_array)[count].v_byte = body_matrix[rowB][offset];
+            (*matrix_array)[count].u_byte = body_matrix[rowB][offset+1];
+
+            offset += 2;
+            count++;
+            squares--;
+        }
+    }
+    return 0;
+}
+
---
+++ src/includes/bitmap.h
@@ -0,0 +1,47 @@
+#ifndef _BITMAP_H_
+#define _BITMAP_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#pragma pack(1)
+typedef struct {
+    uint16_t signature;         // 'BM'
+    uint32_t size;              // File size in bytes
+    uint32_t reserved;          // unused (=0)
+    uint32_t data_offset;       // Offset from beginning of file to the beginning of the bitmap data
+} Header;
+
+#pragma pack(1)
+typedef struct {
+    uint32_t info_header_size;  // Size of InfoHeader =40 
+    int32_t  width_px;          // Horizontal width of bitmap in pixels
+    int32_t  height_px;         // Vertical height of bitmap in pixels
+    uint16_t planes;        // Number of Planes (=1)
+    uint16_t bits_per_pixel;    // Bits per pixel
+    uint32_t compression_type;       // Type of Compression  
+    uint32_t image_size;  // (compressed) Size of Image
+    int32_t  x_resolution;  // horizontal resolution: Pixels/meter
+    int32_t  y_resolution;  // vertical resolution: Pixels/meter
+    uint32_t num_colors;        // Number of actually used colors.
+    uint32_t num_colors_important;  // Number of important colors 
+} InfoHeader;
+
+typedef struct {
+    char filename[256];
+    Header header;
+    InfoHeader info_header;
+    uint8_t* until_offset;
+    uint8_t* body;
+} BMPImage;
+
+
+int read_image(FILE *fp, BMPImage *image, char * filename);
+
+int fill_matrix_array(BMPImage *image, mat_representation ** mat_array, size_t * arr_size);
+
+int fill_body_from_matrix(BMPImage *image, uint8_t  ** body , mat_representation *mat_array, size_t arr_size);
+
+
+#endif
